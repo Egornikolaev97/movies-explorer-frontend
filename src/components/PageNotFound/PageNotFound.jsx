af03c49d082d8e310e6e2d6fe68a48d51004bf9b
Egor Nikolaev <egornikolaev97@yandex.ru>
2023-01-24 21:54:13 +0600

    готовая работа

--- src/components/PageNotFound/PageNotFound.jsx
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -1,6 +1,6 @@
 import './PageNotFound.css';
 import React from 'react';
-import { NavLink, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const PageNotFound = () => {
   const navigate = useNavigate();
@@ -10,9 +10,6 @@ const PageNotFound = () => {
         <h1 className='notfound__code'>404</h1>
         <p className='notfound__type'>Страница не найдена</p>
       </div>
-      {/* <NavLink className='notfound__link' to='/'>
-        Назад
-      </NavLink> */}
       <button className='notfound__btn' onClick={() => navigate(-1)}>
         Назад
       </button>
